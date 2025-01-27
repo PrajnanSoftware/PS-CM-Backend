2cdef80cd15235e924e168d95dc84ac3e5090861
gagansdevang <gagansdevang.14@gmail.com>
2025-01-27 21:25:04 +0530

    ipfs change

--- utils/ipfsClient.js
+++ utils/ipfsClient.js
@@ -1,20 +1,21 @@
-
 import { create } from 'ipfs-http-client';
 
 const ipfs = create({
   url: 'https://ipfs.infura.io:5001/api/v0',
-  fetch: async (url, options) => {
-    return fetch(url, {
-      ...options,
-      duplex: 'half', // Explicitly set the duplex option
-    });
+  fetch: async (url, options = {}) => {
+    // Ensure the duplex option is explicitly set when sending a body
+    if (options.body && typeof options.body === 'object') {
+      options.duplex = 'half';
+    }
+    return fetch(url, options);
   },
 });
 
 export const uploadToIPFS = async (fileBuffer) => {
   try {
     const { path } = await ipfs.add(fileBuffer);
-    return path; // Returns the CID of the file
+    console.log('File uploaded to IPFS with CID:', path);
+    return path; // Return the CID of the uploaded file
   } catch (error) {
     console.error('Error uploading to IPFS:', error);
     throw error;
